08966e843f1f128b4e764513f3d6d668e4674de3
giuliatalamonti <g.talamonti@itsrizzoli.it>
2023-03-14 18:32:35 +0100

    fixes typo and adds comment

--- 1/1-js.js
+++ 1/1-js.js
@@ -1,34 +1,39 @@
 
-var n = 1;
+
 
 // Look at the difference of window object and document object
 console.log(window)
 console.log(document)
 
+// get the element by id anchor-1 and store it in a variable
 const anchor = document.getElementById('anchor-1')
-
+// initialize a variable n as a counter
+var n = 1;
+// handle the click event on the anchor element
 function onClick() {
-  n = n + 1;
   alert(`Hello you have clicked on the anchor ${n} times ${Intl.DateTimeFormat().format()}`)
+  n = n + 1;
 }
-
+// attach the event listener to the anchor element
 anchor.addEventListener('click', onClick)
 
-const options = {
+
+// Date format options 
+const longMonthDateFormat = {
   year: "numeric",
   month: "long",
   day: "numeric",
 }
 
-// insert a dynimic value in the html element p 
-const htmlTagForDate = document.getElementById('date').innerHTML = Intl.DateTimeFormat('it-IT', options).format()
 
 // create a new span element
-const child = document.createElement('span')
-// get the h1 element by id
-const h1 = document.getElementById('maintitle')
-// append a child to the h1 element
-h1.appendChild(child).innerHTML = Intl.DateTimeFormat('it-IT', options).format()
+const newElement = document.createElement('span')
+// get the h1 element
+const body = document.getElementsByTagName('h1')[0]
+// attach after the h1 element
+body.after(newElement)
+
+newElement.innerHTML = Intl.DateTimeFormat('it-IT', longMonthDateFormat).format()
 
 
 
