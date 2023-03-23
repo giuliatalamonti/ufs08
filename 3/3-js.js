cada4c0d143aa3cfe46adadd2d475ca94c7ebd10
giuliatalamonti <g.talamonti@itsrizzoli.it>
2023-03-23 15:41:37 +0100

    updates instructions

--- 3/3-js.js
+++ 3/3-js.js
@@ -7,3 +7,8 @@ const LosAngelesCelsius = 15
 // 2. Aggiungi il valore delle varie temperature a ogni elemento dell'HTML che trovi con l'id temperatureItaly, temperatureThailand, temperatureUSA
 // 3. Le temperature mostrate nell'HTML dovranno avere anche il relativo simbolo di misura (°C, °F, K)
 // 4. Arrotonda per eccesso le temperature in gradi Fahrenheit e Kelvin (hint: usa la funzione Math.ceil())
+// 5. Metti in evidenza con colori diversi le temperature nei vari gradi
+// 6. Implementa al click della città il redirect sul sito della meteo di quella città (esempio: https://www.meteo.it/)
+// ADVANCED
+// 7. aggiungi un'immagine di sfondo che rappresenti la città selezionata
+// 8. aggiungi un menù a tendina con cui mostrare in base alla scelta la temperatura in gradi Celsius, Fahrenheit o Kelvin
