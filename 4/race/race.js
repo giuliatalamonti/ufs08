5778727e2db20d183432dd4c64768d04bd9f37c5
giuliatalamonti <g.talamonti@itsrizzoli.it>
2023-03-27 11:39:31 +0200

    adds exercise race

---
+++ 4/race/race.js
@@ -0,0 +1,19 @@
+// Race day registration
+// Here’s how our registration works. There are adult runners (over 18 years of age) and youth runners (under 18 years of age). They can register early or late.
+// Runners are assigned a race number and start time based on their age and registration.
+
+// Race number:
+
+// Early adults receive a race number at or above 1000.
+// All others receive a number below 1000.
+// Start time:
+
+// Adult registrants run at 9:30 am or 11:00 am.
+// Early adults run at 9:30 am.
+// Late adults run at 11:00 am.
+// Youth registrants run at 12:30 pm (regardless of registration).
+
+// But we didn’t plan for runners that are exactly 18! We’ll handle that by the end of the project.
+
+// 1. crea nell'html un elemento di tipo input di tipo testo per poter inserire il nome del runner, e l'età del runner, e un bottone di conferma
+// 2. al click sul bottone di conferma, si deve mostrare un alert con il numero di partenza e l'orario di partenza del runner
