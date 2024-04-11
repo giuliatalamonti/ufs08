4da7b3674202261fec20830b921cfc47df1d0150
giuliatalamonti <g.talamonti@itsrizzoli.it>
2024-04-11 13:50:11 +0200

    adds instructions ex lesson03

---
+++ 03/3-js.js
@@ -0,0 +1,14 @@
+// Nel mondo oggi ci sono queste temperature per Milano, Bangkok e Los Angeles in gradi Celsius.
+const MilanCelsius = 20
+const BangkokCelsius = 35
+const LosAngelesCelsius = 15
+
+// 1. Scrivi il codice che converte le temperature in gradi Fahrenheit e Kelvin
+// 2. Aggiungi il valore delle varie temperature a ogni elemento dell'HTML che trovi con l'id temperatureItaly, temperatureThailand, temperatureUSA
+// 3. Le temperature mostrate nell'HTML dovranno avere anche il relativo simbolo di misura (°C, °F, K)
+// 4. Arrotonda per eccesso le temperature in gradi Fahrenheit e Kelvin (hint: usa la funzione Math.ceil())
+// 5. Metti in evidenza con colori diversi le temperature nei vari gradi
+// 6. Implementa al click della città il redirect sul sito della meteo di quella città (esempio: https://www.meteo.it/)
+// ADVANCED
+// 7. aggiungi un'immagine di sfondo che rappresenti la città selezionata
+// 8. aggiungi un menù a tendina con cui mostrare in base alla scelta la temperatura in gradi Celsius, Fahrenheit o Kelvin
