69a5cb457337ff5a8e42a8a68e76256b1ec85e80
giuliatalamonti <g.talamonti@itsrizzoli.it>
2024-05-13 20:51:57 +0200

    adds solution

--- 10/async/exercise.js
+++ 10/async/exercise.js
@@ -18,4 +18,85 @@
 // console.log(alphabet);
 // should output abcd
 
+// Soluzione
+function forEach(arr, fn) {
+  for (let i = 0; i < arr.length; i++) {
+    fn(arr[i]);
+  }
+}
+
+function map(arr, fn) {
+  let n = []
+  for (let i = 0; i < arr.length; i++) {
+    n.push(fn(arr[i]))
+  }
+  return n
+}
+
+function executeOnElement(element) {
+  console.log(element)
+}
+
+forEach(['pizza', 'pasta'], executeOnElement);
+
+// ---
+const letters = ["a", "b", "c", "d"];
+let alphabet = "";
+
+forEach(letters, function (char) {
+  alphabet += char;
+});
+
+console.log('al', alphabet)
+
 // Another reimplementation of a native Array method. Notice the difference with map, map returns an array, forEach doesn't return anything so whatever needs to happen needs to take place in the body of the callback function.
+
+// PROMISE
+
+// const getPockemon = fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20")
+
+// getPockemon.then((resultOk) => {
+//   resultOk.json().then((result) => {
+//     console.log(result.results)
+//   })
+// }).catch((error) => {
+//   console.error(error)
+// })
+
+
+// const apromise = new Promise((resolve, reject) => {
+//   const value = Math.floor(Math.random(1) * 1000)
+//   let isOk = value % 2 === 0
+//   if(isOk) resolve(value)
+//   reject('Oh no!')
+// })
+
+// apromise.then((r) => {
+//  console.log(r)
+// }).catch((e) => {
+//   console.log(e)
+// })
+
+// ASYNC AWAIT
+const body = document.querySelector('body')
+async function getPokemonList() {
+  try {
+     const result = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20")
+
+     const json = await result.json()
+
+     json.results.map((pokemon) => {
+       const eleHtml = document.createElement('p')
+       eleHtml.innerText = pokemon.name
+       body.appendChild(eleHtml)
+     })
+
+
+     return json
+  } catch(e) {
+    throw new Error(e)
+  }
+}
+
+
+getPokemonList()
