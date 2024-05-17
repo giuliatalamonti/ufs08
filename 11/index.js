dc9192ee5cea832e8c9b4c243ad730955da3950d
giuliatalamonti <g.talamonti@itsrizzoli.it>
2024-05-17 17:17:05 +0200

    update exercise

--- 11/index.js
+++ 11/index.js
@@ -6,6 +6,16 @@ const rangeValue = document.getElementById('rangeValue')
 rangeValue.innerText = rangeElement.value
 
 // on change
-rangeElement.addEventListener('change',(event) => {
+rangeElement.addEventListener('change', (event) => {
   rangeValue.innerText = event.target.value
+})
+
+// COLOR PICKER
+const bdElement = document.querySelector('body')
+
+const colorPicker = document.querySelector('#colorPicker')
+
+colorPicker.addEventListener('change', (event) => {
+  const newColor = event.target.value
+  bdElement.style.setProperty('--bg-color', newColor)
 })
