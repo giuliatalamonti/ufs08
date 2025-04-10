6655e53512241396d064a925b927fee831571066
giuliatalamonti <g.talamonti@itsrizzoli.it>
2025-04-10 11:48:24 +0200

    add exercise

--- 03/index.js
+++ 03/index.js
@@ -18,3 +18,46 @@ h1Element.after(h2Element)
 // h1Element.append(h2Element) -> viene inserito come figlio di h1
 // h1Element.insertAdjacentHTML()
 // h1Element.before()
+
+// TODO
+// Somma / Sottrazione / Divisione / Moltiplicazione
+
+const temp = 12
+const temp2 = 10
+
+const sumResult = temp + temp2
+
+document.getElementById('somma').innerText = sumResult
+
+const substractionResult = temp - temp2
+const divideResult = temp / temp2
+
+document.getElementById('divisione').innerText = divideResult
+
+// Dichiarare un BigInt
+
+const bigIntExample = BigInt(9000000999999999999999999999999977)
+
+const bigIntExample2 = 9000000999999999999999999999999977n
+
+console.log(bigIntExample)
+console.log(bigIntExample2)
+
+// Math utility - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+// Concatenare due stringhe
+const str1 = 'una stringa'
+const str2 = "e un'altra ancora"
+
+const strResult = str1 + '\n - \n' + str2
+
+const strResult2 = `${str1} 
+
+-
+
+${str2}`
+
+console.log(strResult)
+console.log(strResult2)
+
+document.getElementById('example-string').innerText = strResult2
