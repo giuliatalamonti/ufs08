efbbccd6181d3b8337c14e67538773eb508cb55e
giuliatalamonti <g.talamonti@itsrizzoli.it>
2025-05-15 09:54:43 +0200

    add exercise

---
+++ 10/async/index.js
@@ -0,0 +1,19 @@
+// Exercise: callback
+// IT
+// Implementate una funzione che si chiama forEach che ha come argomenti:
+// - un array
+// - una callback (funzione)
+// La callback viene eseguita su ogni elemento dell' array
+// Il forEach non ritorna nulla
+
+// EN
+// The function forEach takes an array and a callback, and runs the callback on each element of the array. forEach does not return anything.
+
+// Esempio (Example):
+// let alphabet = "";
+// const letters = ["a", "b", "c", "d"];
+// forEach(letters, function (char) {
+//   alphabet += char;
+// });
+// console.log(alphabet);
+// should output abcd
